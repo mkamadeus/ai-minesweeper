d02b6a3af41a2c7667a62cdff970d2a4dda49896
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-11-22 01:04:06 +0700

    GUI for playing minesweeper

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,31 +1,33 @@
 from typing import Tuple
+from enum import Enum
 import random
 
 
 class Minesweeper:
-    def __init__(self, size: int = 10):
+    def __init__(self, size: int = 10, bombs: int = 10):
         self.board = [[Tile(-1, False, False) for _ in range(size)]
                       for _ in range(size)]
         self.size = size
+        self.bombs = bombs
+        self.status = MinesweeperStatus.PLAYING
 
-    def initialize_board(self, starting_position: Tuple[int, int], bombs: int = 10):
+    def initialize_board(self, starting_position: Tuple[int, int]):
         '''
         Initialize board after click on starting position.
         '''
 
-        for _ in range(bombs):
+        for _ in range(self.bombs):
             r, c = starting_position
-            while(starting_position == (r, c)):
+            while(starting_position == (r, c) or self.board[r][c].is_bomb):
                 r, c = random.randint(
                     0, self.size - 1), random.randint(0, self.size - 1)
-            print((r, c))
             self.board[r][c].is_bomb = True
 
         self.reveal(starting_position)
 
     def is_tile_valid(self, coor: Tuple[int, int]) -> bool:
         '''
-        Check whether tile is valid (not out of bounds). 
+        Check whether tile is valid (not out of bounds).
         '''
 
         return ((coor[0] >= 0) and (coor[0] < self.size)) and ((coor[1] >= 0) and (coor[1] < self.size))
@@ -35,12 +37,23 @@ class Minesweeper:
         Does BFS traversal to reveal tile status.
         '''
 
+        r, c = current_position
+
+        if(self.board[r][c].status != -1):
+            return
+
+        # Check if position (r, c) is bomb...
+        if(self.board[r][c].is_bomb):
+            self.status = MinesweeperStatus.LOSE
+            return
+
         offset = [(-1, -1), (-1, 0), (-1, 1), (0, -1),
                   (0, 1), (1, -1), (1, 0), (1, 1)]
-        queue = []
-        visited = [[False for _ in range(self.size)]
-                   for _ in range(self.size)]
 
+        visited = [[False if self.board[i][j].status == -1 else True for i in range(self.size)]
+                   for j in range(self.size)]
+        visited[r][c] = True
+        queue = []
         queue.append(current_position)
 
         while(len(queue) != 0):
@@ -52,6 +65,7 @@ class Minesweeper:
             bomb_count = self.count_bomb_around((r, c))
             self.board[r][c].status = bomb_count
 
+            print(bomb_count)
             # If no bombs, do BFS
             if(bomb_count == 0):
                 for dr, dc in offset:
@@ -94,3 +108,9 @@ class Tile:
         self.status = status
         self.is_bomb = is_bomb
         self.is_marked = is_marked
+
+
+class MinesweeperStatus(Enum):
+    PLAYING = 1
+    LOSE = 2
+    WIN = 3
--- gui.py
+++ gui.py
@@ -1,13 +1,16 @@
 import PySimpleGUI as gui
 from PySimpleGUI.PySimpleGUI import WINDOW_CLOSED
+from Minesweeper import Minesweeper, MinesweeperStatus
 
+minesweeper = Minesweeper(size=8, bombs=5)
+is_initialized = False
 
 layout = [
-    [gui.Text('Title')],
-    *[[gui.Button(f'{i},{j}', key=f'tile_{i}_{j}')
-       for j in range(10)] for i in range(10)],
-    [gui.Text('Title')]
+    [gui.Text('Minesweeper')],
+    *[[gui.Button(key=f'tile_{i}_{j}')
+       for j in range(minesweeper.size)] for i in range(minesweeper.size)],
 ]
+
 
 window = gui.Window('Minesweeper', layout)
 
@@ -15,6 +18,32 @@ while True:
     event, values = window.read()
     if(event == WINDOW_CLOSED):
         break
-    print(event)
+
+    if(event is not None and 'tile' in event):
+        r, c = list(map(lambda v: int(v), event.split('_')[1:]))
+
+        print(r, c, minesweeper.board[r][c].is_bomb)
+
+        if(not is_initialized):
+            minesweeper.initialize_board((r, c))
+            is_initialized = True
+        else:
+            minesweeper.reveal((r, c))
+
+        if(minesweeper.status == MinesweeperStatus.LOSE):
+            gui.popup('Cupu kon cok')
+            break
+
+        for i in range(minesweeper.size):
+            for j in range(minesweeper.size):
+                status = ''
+                if(minesweeper.board[i][j].status != -1):
+                    if(minesweeper.board[i][j].is_bomb):
+                        status = 'B'
+                    else:
+                        status = str(minesweeper.board[i][j].status)
+                window[f'tile_{i}_{j}'].update(status)
+
+        minesweeper.print_board()
 
 window.close()
